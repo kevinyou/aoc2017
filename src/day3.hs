fa0e11d6ea44788f9e3dbd86a1ea4c2378ddf668
Kevin You <kevin.you@outlook.com>
2017-12-27 18:58:30 -0600

    cleaned up day3

--- src/day3.hs
+++ src/day3.hs
@@ -1,42 +1,36 @@
 import Data.List.Split (splitOn)
 
--- generating directions
-interleave (x:xs) (y:ys) = x : y : interleave xs ys
-alt = 1 : map ((-1)*) alt
-zeroes = [0,0..]
-unweighted_dirs = zip (tail (interleave zeroes alt)) (interleave zeroes (tail alt))
-doubled = interleave [1,2..] [1,2..]
-dirs' = [(w*x, w*y) | ((x,y), w) <- zip unweighted_dirs doubled]
 
-unwrap ((x,0):xs) =
-    case abs x of
-        1 -> (x,0) : unwrap xs
-        _ -> case compare x 0 of
-            LT -> (-1,0) : unwrap ((x+1,0) : xs)
-            GT -> (1,0) : unwrap ((x-1,0) : xs)
-unwrap ((0,x):xs) =
-    case abs x of
-        1 -> (0,x) : unwrap xs
-        _ -> case compare x 0 of
-            LT -> (0,-1) : unwrap ((0,x+1) : xs)
-            GT -> (0,1) : unwrap ((0,x-1) : xs)
+-- cycles east, north, west, south, ...
+unweighted_dirs = cycle [(1,0), (0,-1), (-1,0), (0,1)]
 
-dirs = unwrap dirs'
+-- places for spiral
+-- e.g. east 1, north 1, west 2, south 2, east 3, north 3, west 4..
+dirs = concat [replicate n pair | (n, pair) <- zip doubled unweighted_dirs]
+    where doubled = concat $ map (replicate 2) [1,2..]
+
 
 -- more helpers
 tupleAdd (x0, y0) (x1, y1) = (x0+x1, y0+y1)
-partialSum (x:xs) = x : (map (tupleAdd x) (partialSum xs))
 
 -- part a
-grid = (1, (0,0)) : (zip [2,3..] dirs)
+
+-- place n at cursor, then place n+1 at cursor+d
+place n cursor (d:ds) = (n, cursor) : (place (succ n) (tupleAdd cursor d) ds)
+    where newCursor = tupleAdd cursor d
+
+grid = place 1 (0,0) dirs
+
+-- part b
 myfind (ox, oy) stored =
     case lookup (ox, oy) stored of
         Just v -> v
         Nothing -> 0
 
--- part b
+place' cursor (d:ds) = cursor : (place' (tupleAdd cursor d) ds)
+    where newCursor = tupleAdd cursor d
 
-spiral = (0,0) : (partialSum dirs)
+spiral = place' (0,0) dirs
 
 adjacent (ox, oy) = [(ox+x, oy+y) | x <- [-1,0,1], y <- [-1,0,1]]
 
@@ -45,13 +39,10 @@ combined ((ox, oy):rest) stored =
         pair = ((ox, oy), sum values) in
         pair : (combined rest (pair : stored))
 
-locAt n ((pos, (dx,dy)):xs) = 
-    if pos == n then (dx,dy)
-    else let (x,y) = locAt n xs in
-        (x+dx, y+dy)
-
-day3a n = let (x,y) = locAt n grid in
+day3a n = let Just (x,y) = lookup n grid in
     abs x + abs y
+
+-- TODO: rewrite with some sort of mutable?
 day3b n = let mygrid = combined spiral [((0,0), 1)] in
     head $ filter (>n) $ map snd mygrid
 
