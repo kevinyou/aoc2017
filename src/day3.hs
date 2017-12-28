eeefcb7494fce3c1bc726c56e6f94236a9bce803
Kevin You <kevin.you@outlook.com>
2017-12-27 22:52:59 -0600

    Clean up day3 and day4 (use foldr)

--- src/day3.hs
+++ src/day3.hs
@@ -1,6 +1,5 @@
 import Data.List.Split (splitOn)
 
-
 -- cycles east, north, west, south, ...
 unweighted_dirs = cycle [(1,0), (0,-1), (-1,0), (0,1)]
 
@@ -9,42 +8,30 @@ unweighted_dirs = cycle [(1,0), (0,-1), (-1,0), (0,1)]
 dirs = concat [replicate n pair | (n, pair) <- zip doubled unweighted_dirs]
     where doubled = concat $ map (replicate 2) [1,2..]
 
-
 -- more helpers
 tupleAdd (x0, y0) (x1, y1) = (x0+x1, y0+y1)
 
 -- part a
-
 -- place n at cursor, then place n+1 at cursor+d
-place n cursor (d:ds) = (n, cursor) : (place (succ n) (tupleAdd cursor d) ds)
-    where newCursor = tupleAdd cursor d
-
 grid = place 1 (0,0) dirs
+    where place n cursor (d:ds) = (n, cursor) : (place (succ n) (tupleAdd cursor d) ds)
 
 -- part b
-myfind (ox, oy) stored =
-    case lookup (ox, oy) stored of
+getValue (x, y) stored =
+    case lookup (x, y) stored of
         Just v -> v
         Nothing -> 0
 
-place' cursor (d:ds) = cursor : (place' (tupleAdd cursor d) ds)
-    where newCursor = tupleAdd cursor d
+adjacent (x, y) = [(x+dx, y+dy) | dx <- [-1,0,1], dy <- [-1,0,1]]
 
-spiral = place' (0,0) dirs
-
-adjacent (ox, oy) = [(ox+x, oy+y) | x <- [-1,0,1], y <- [-1,0,1]]
-
-combined ((ox, oy):rest) stored =
-    let values = [value | (x,y) <- (adjacent (ox,oy)), let value = myfind (x,y) stored]
-        pair = ((ox, oy), sum values) in
-        pair : (combined rest (pair : stored))
+placeAdj cursor (d:ds) stored = (cursor,  n) : (placeAdj (tupleAdd cursor d) ds ((cursor, n) : stored))
+    where n = sum [value | value <- map (flip getValue stored) (adjacent cursor)]
+gridAdj = placeAdj (0,0) ((0,0) : dirs) [((0,0), 1)]
 
 day3a n = let Just (x,y) = lookup n grid in
     abs x + abs y
 
--- TODO: rewrite with some sort of mutable?
-day3b n = let mygrid = combined spiral [((0,0), 1)] in
-    head $ filter (>n) $ map snd mygrid
+day3b n = head $ filter (>n) $ map snd $ gridAdj
 
 main = do
     rawStr <- getContents
--- src/day4.hs
+++ src/day4.hs
@@ -1,14 +1,13 @@
 import Data.List.Split (splitOn)
 import Data.List (sort, nubBy)
 
-count (x:xs) dict =
-    case lookup x dict of
-        Just val -> (x, val+1) : count xs ((x, val+1) : dict)
-        Nothing -> (x, 1) : count xs ((x, 1) : dict)
-count [] dict = []
+counts = foldr (\x acc -> count x acc : acc) []
+    where count x dict = case lookup x dict of
+            Just val -> (x, val+1)
+            Nothing -> (x, 1)
 
-removeDup = nubBy (\a -> \b -> fst a == fst b)
-valid xs = (length $ removeDup $ count xs []) == (length xs)
+removeDup = nubBy (\a b -> fst a == fst b)
+valid xs = (length $ removeDup $ counts xs) == (length xs)
 
 day4a xss = length $ filter valid xss
 day4b xss = day4a $ map (map sort) xss
